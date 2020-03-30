11ebd459c8927beba0cd2b4adc1564964fbcd151
Janelle Kuhns <j3ckuhns@gmail.com>
2020-03-30 16:51:22 -0700

    no errors with flake8

--- logpuzzle.py
+++ logpuzzle.py
@@ -12,7 +12,9 @@ http://code.google.com/edu/languages/google-python-class/
 Given an apache logfile, find the puzzle urls and download the images.
 
 Here's what a puzzle url looks like:
-10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
+10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg
+HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US;
+rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 
 """
 
@@ -36,12 +38,10 @@ def read_urls(filename):
                 url_match = re.search(r'\S+puzzle+\S+jpg', line)
                 if url_match:
                     url_match_list.append(url_match.group())
-    completed_url_list = sorted(list(set(url_match_list)), key=lambda url: url[-8:-4])
+    completed_url_list = sorted(list(set(url_match_list)),
+                                key=lambda url: url[-8:-4])
     print('My list is sorted')
     return completed_url_list
-
-
-
 
 
 def download_images(img_urls, dest_dir):
@@ -70,9 +70,10 @@ def download_images(img_urls, dest_dir):
             + ".jpeg"
         )
 
+        image_tags = ""
         image_tags += """
         <img src='./{0}/img{1}{2}' />
-        """.format( dest_dir, str(image), ".jpeg")
+        """.format(dest_dir, str(image), ".jpeg")
 
     html = """
         <html>
@@ -84,14 +85,13 @@ def download_images(img_urls, dest_dir):
         """.format(image_tags)
     with open("", "w") as f:
         f.write(html)
-    
-
 
 
 def create_parser():
     """Create an argument parser object"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--todir', help='destination direction for downloaded images')
+    parser.add_argument('-d', '--todir',
+                        help='destination direction for downloaded images')
     parser.add_argument('logfile', help='apache logfile to extract urls from')
 
     return parser
