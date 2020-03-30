5e26c0f949431b69caac74b611e3edfe7298471b
Janelle Kuhns <j3ckuhns@gmail.com>
2020-03-30 16:52:22 -0700

    added author statement

--- logpuzzle.py
+++ logpuzzle.py
@@ -17,6 +17,7 @@ HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US;
 rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 
 """
+__author__ = "Janelle Kuhns with lots of googling"
 
 import os
 import re
